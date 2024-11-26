51d48c522f6582e2f6086240a9dd778937b213eb
Anastasia <maly1204n@gmail.com>
2024-11-26 20:19:02 +0300

    True Empty

---
+++ trueEmpty/trueEmpty.js
@@ -0,0 +1,17 @@
+function isEmpty(obj) {
+    if (Array.isArray(obj)) {
+        return obj.length === 0;
+    }
+    
+    if (typeof obj === 'object' && obj !== null) {
+        return Object.keys(obj).length === 0; 
+    }
+    
+    return false;
+}
+
+//Проверка
+console.log(isEmpty({ "x": 5, "y": 42 })); //false
+console.log(isEmpty({})); //true
+console.log(isEmpty([null, false, 0])); //false
+console.log(isEmpty([])); //true
